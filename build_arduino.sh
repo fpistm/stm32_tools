1ae3a53490218f2cb64251af79145493fbbf4dd7
Frederic.Pillon <frederic.pillon@st.com>
2017-08-10 17:57:26 +0200

    Add option to build all sketch files found
    Signed-off-by: Frederic.Pillon <frederic.pillon@st.com>

--- build_arduino.sh
+++ build_arduino.sh
@@ -25,6 +25,7 @@ NB_BUILD=0
 VERSION="0.1"
 LOG_FILE=/tmp/build_arduino_`date +\%d_\%m_\%Y_\%H_\%M`.log
 INO_FILE=examples/01.Basics/Blink/Blink.ino
+ALL_INO=0
 
 ###############################################################################
 ## Help function
@@ -37,7 +38,7 @@ usage()
     echo "############################################################"
     echo "##"
     echo "## `basename $0`"
-    echo "## [-v] [-s <sketch .ino path>]"
+    echo "## [-a] [-s <sketch .ino path>][-v] "
     echo "##"
     echo "## Launch this script at the top of Arduino IDE directory."
     echo "##"
@@ -47,6 +48,7 @@ usage()
     echo "##"
     echo "## Optionnal:"
     echo "##"
+    echo "## -a: build all sketch found."
 	echo "## -s <sketch filepath>: ino file to build (default: $INO_FILE)"
     echo "## -v: print version"
     echo "##"
@@ -78,21 +80,50 @@ print_stat() {
    echo ""
 }
 
+build_all() {
+  local Sketch=$INO_FILE
+  if [ $# -ne 0 ]; then
+    Sketch=$1
+  fi
+  echo "Sketch: $Sketch" >> $LOG_FILE
+  echo -e "Sketch: \033[1;36m$Sketch\033[0m"
+
+  build NUCLEO_F030R8 Nucleo_64 $Sketch
+  build NUCLEO_F091RC Nucleo_64 $Sketch
+  build NUCLEO_F103RB Nucleo_64 $Sketch
+  build NUCLEO_F207ZG Nucleo_144 $Sketch
+  build NUCLEO_F303RE Nucleo_64 $Sketch
+  build NUCLEO_F401RE Nucleo_64 $Sketch
+  build NUCLEO_F411RE Nucleo_64 $Sketch
+  build NUCLEO_F429ZI Nucleo_144 $Sketch
+
+  build NUCLEO_L053R8 Nucleo_64 $Sketch
+  build NUCLEO_L432KC Nucleo_32 $Sketch
+  build NUCLEO_L476RG Nucleo_64 $Sketch
+
+  build DISCO_F100RB Disco $Sketch
+  build DISCO_F407VG Disco $Sketch
+  build DISCO_F746NG Disco $Sketch
+
+  #build BLUEPILL_F103C8 Other $sketch_file
+  #build MAPLEMINI_F103CB Other $sketch_file
+}
 
 build() {
   local Target=$1
   local Pack=$2
+  local Sketch=$3
   echo "build $Target" >> $LOG_FILE
   echo -ne "build \033[1;34m$Target\033[0m ..."
   #ex: ./arduino --board STM32:stm32:Nucleo_144:Nucleo_144_board=NUCLEO_F429ZI --verify $INO_FILE  >> $LOG_FILE 2>&1
-  ./arduino --board STM32:stm32:${Pack}:board_part_num=$Target --verify $INO_FILE  >> $LOG_FILE 2>&1
+  ./arduino --board STM32:stm32:${Pack}:board_part_num=$Target --verify $Sketch  >> $LOG_FILE 2>&1
   check_result $? $Target
   NB_BUILD=$((NB_BUILD+1))
 }
 
 # parse command line arguments
 # options may be followed by one colon to indicate they have a required arg
-options=`getopt -o s:hv -- "$@"`
+options=`getopt -o as:hv -- "$@"`
 
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 
@@ -100,6 +131,9 @@ eval set -- "$options"
 
 while true ; do
     case "$1" in
+    -a) echo "Build all sketches"
+		ALL_INO=1
+        shift;;
 	-s) echo "Sketch to build: $2"
         INO_FILE=$2
         shift 2;;
@@ -120,31 +154,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if [ ! -f $INO_FILE ]; then
-	echo "$INO_FILE does not exist!"
+if [ $ALL_INO -eq 0 ]; then
+  if [ ! -f $INO_FILE ]; then
+    echo "$INO_FILE does not exist!"
     exit 2
+  fi
+  build_all
+else
+  list=(`find portable  examples libraries -name "*.ino"`)
+  if [ ${#list[@]} -ne 0 ]; then
+    echo "Number of ino files found: ${#list[@]}"
+  else
+    echo "No ino files found!"
+    exit 3
+  fi
+  for i in ${list[@]}
+  do
+    build_all $i
+  done
 fi
-
-build NUCLEO_F030R8 Nucleo_64
-build NUCLEO_F091RC Nucleo_64
-build NUCLEO_F103RB Nucleo_64
-build NUCLEO_F207ZG Nucleo_144
-build NUCLEO_F303RE Nucleo_64
-build NUCLEO_F401RE Nucleo_64
-build NUCLEO_F411RE Nucleo_64
-build NUCLEO_F429ZI Nucleo_144
-
-build NUCLEO_L053R8 Nucleo_64
-build NUCLEO_L432KC Nucleo_32
-build NUCLEO_L476RG Nucleo_64
-
-build DISCO_F100RB Disco
-build DISCO_F407VG Disco
-build DISCO_F746NG Disco
-
-#build BLUEPILL_F103C8 Other
-#build MAPLEMINI_F103CB Other
-
 
 print_stat
 
